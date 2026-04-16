3787b199aebefdd9dabf058d7a19b2667e947da3
AikiddoX0 <aikiddox@gmail.com>
2026-04-16 22:55:24 +0530

    Update save.php

--- save.php
+++ save.php
@@ -1,1 +1,33 @@
+<?php
 
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "chess";
+
+$con = mysqli_connect($server ,$username ,$password ,$dbname);
+
+if(!$con)
+{
+  echo "not connected";
+}
+
+$name = $_POST['full_name'];
+$Number = $_POST['whatsapp'];
+$Username = $_POST['username'];
+$Payment_ID = $_POST['payment_id'];
+
+$sql = "INSERT INTO `text`(`Full Name`, `WhatsApp Number`, `Lichess / Chess.com Username`, `Payment Transaction ID`) VALUES ('$name','$Number','$Username','$Payment_ID')";
+
+$result = mysqli_query($con ,$sql);
+
+if($result)
+{
+  echo "data summeted";
+}
+
+else
+{
+  echo "error...!";
+}
+?>
